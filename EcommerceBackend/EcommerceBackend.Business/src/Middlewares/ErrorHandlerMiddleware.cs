34fc6b8bfcaf3bb532110f7a1432ccf06d76a982
SamAddy <addisonsamuel70@yahoo.com>
2023-08-03 22:17:58 +0300

    Add logging and error handler middleware

--- EcommerceBackend/EcommerceBackend.Business/src/Middlewares/ErrorHandlerMiddleware.cs
+++ EcommerceBackend/EcommerceBackend.Business/src/Middlewares/ErrorHandlerMiddleware.cs
@@ -1,12 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
 namespace EcommerceBackend.Business.src.Middlewares
 {
-    public class ErrorHandlerMiddleware 
+    public class ErrorHandlerMiddleware : IMiddleware
     {
-        
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var errorResponse = new{
+                Message = "An error occurred while processing your request.",
+                ExceptionMessage = exception.Message,
+                ExceptionType = exception.GetType().FullName
+            };
+
+            var jsonErrorREsponse = JsonSerializer.Serialize(errorResponse);
+            await context.Response.WriteAsync(jsonErrorREsponse);
+        }
     }
 }
---
+++ EcommerceBackend/EcommerceBackend.Business/src/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace EcommerceBackend.Business.src.Middlewares
+{
+    public class LoggingMiddleware : IMiddleware
+    {
+        private readonly ILogger<LoggingMiddleware> _logger;
+
+        public LoggingMiddleware(ILogger<LoggingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            // Console.WriteLine($"Incoming request: {context.Request.Path} {context.User.Identities} {context.Request.QueryString} ");
+            // await context.Response.WriteAsync("Request should end here.");
+
+            var originalResponseBody = context.Response.Body;
+
+            using var responseBodyStream = new MemoryStream();
+            context.Response.Body = responseBodyStream;
+
+            try
+            {
+                // var requestInfo = $"{context.Request.Method} {context.Request.Path} {context.Request.QueryString}";
+                // _logger.LogInformation($"Request: {requestInfo}, User-Agent: {context.Request.Headers["User-Agent"]}, Remote IP: {context.Connection.RemoteIpAddress} ");
+                _logger.LogInformation("{Timestamp:yyyy-MM-dd HH:mm:ss.fff} - Request: {Method} {Path}{QueryString}, User-Agent: {UserAgent}, Remote IP: {RemoteIpAddress}",
+                    DateTime.Now, context.Request.Method, context.Request.Path, context.Request.QueryString, context.Request.Headers["User-Agent"], context.Connection.RemoteIpAddress);
+
+                await next(context);
+
+                responseBodyStream.Seek(0, SeekOrigin.Begin);
+                // var responseBody = await new StreamReader(responseBodyStream).ReadToEndAsync();
+                // var responseInfo = $"Status: {context.Response.StatusCode}";
+                _logger.LogInformation("Response: {ResponseInfo}, Content: {ResponseBody}",
+                    context.Response.StatusCode, await new StreamReader(responseBodyStream).ReadToEndAsync());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("An error occurred while processing the request.", ex);
+                throw;
+            }
+            finally
+            {
+                responseBodyStream.Seek(0, SeekOrigin.Begin);
+                await responseBodyStream.CopyToAsync(originalResponseBody);
+            }
+        }
+    }
+}
