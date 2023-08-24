2d1e816345181ab080360c794ab9b5b2355e5871
SamAddy <addisonsamuel70@yahoo.com>
2023-08-24 23:23:10 +0300

    Update get profile from token in authentication controller

--- EcommerceBackend/EcommerceBackend.Application/Controllers/AuthController.cs
+++ EcommerceBackend/EcommerceBackend.Application/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
+using System.Security.Claims;
 using EcommerceBackend.Business.src.Dtos.UserDtos;
 using EcommerceBackend.Business.src.Services.Abstractions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceBackend.Application.Controllers
@@ -9,22 +11,44 @@ namespace EcommerceBackend.Application.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
+        private readonly IUserService _userService;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService authService, IUserService userService)
         {
             _authService = authService;
+            _userService = userService;
         }
 
-        [HttpPost("profile")]
+        [HttpPost("login")]
         public async Task<ActionResult<string>> AuthenticateUser([FromBody] UserCredentialsDto userCredentials)
         {
             return Ok(await _authService.AutheticateUser(userCredentials));
         }
 
         [HttpPost("token")]
+        [Authorize]
         public async Task<ActionResult<string>> RefreshToken([FromBody] string token)
         {
             return Ok(await _authService.RefreshToken(token));
+        }
+
+        [HttpGet("profile")]
+        [Authorize]
+        public async Task<ActionResult<ReadUserDto>> GetUserProfile()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (userIdClaim == null || !Guid.TryParse(userIdClaim.Value, out var userId))
+            {
+                return Unauthorized();
+            }
+
+            var user = await _userService.GetUserByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
         }
     }
 }
