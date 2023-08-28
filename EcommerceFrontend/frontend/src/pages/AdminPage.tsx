fe69deba276d77619eeda678054897948f26969a
SamAddy <addisonsamuel70@yahoo.com>
2023-08-28 23:13:35 +0300

    Add admin protected routes

---
+++ EcommerceFrontend/frontend/src/pages/AdminPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AdminPanel from '../components/AdminPanel'
+import Header from '../components/Header'
+
+const AdminPage = () => {
+  return (
+    <div>
+        <AdminPanel />
+    </div>
+  )
+}
+
+export default AdminPage
